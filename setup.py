a3e6c3ef67979b7888ffc04f924d396e5f7334ab
Ronan Jouchet <ronan@jouchet.fr>
2014-07-03 22:10:12 -0400

    Bump version to 0.3.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def readme():
         return f.read()
 
 setup(name='whoarder',
-    version='0.2.1',
+    version='0.3.0',
     description="whoarder converts your Kindle's 'My Clippings.txt' file to a more pleasant, sortable, filterable HTML file",
     long_description=readme(),
     classifiers=[
